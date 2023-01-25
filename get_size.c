765a04b4e32d1993bc209a697bd36fab07fe7ea8
Viclins_Toriel <gabtorielvix@gmail.com>
2023-01-24 19:35:15 -0800

    Added comments to some functions

--- get_size.c
+++ get_size.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * @i: List of arguments to be printed(int).
  *
- * Return: Precision.
+ * Return: Precision(int).
  */
 int get_size(const char *format, int *i)
 {
